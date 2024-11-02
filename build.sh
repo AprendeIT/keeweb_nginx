dde96629b989aa5ba1485831d5d5754b4f77e1bf
Gerardo G.U. <gerardo.garcia.urtiaga@gmail.com>
2024-11-02 18:11:43 +0100

    Adding all the files for the first commit

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t "keeweb_nginx:2.0" .
+docker build -t "keeweb_nginx:latest" .
--- create_container.sh
+++ create_container.sh
@@ -1,1 +1,1 @@
-docker run -it -d --restart always -e "SSL_DOMAIN=TESTING.COM" -e "IP_WEBDAV=10.1.32.200" -e "HT_USER=put_your_user_here" -e "HT_PASSWORD=put_your_password_here" -p 10.1.32.200:443:443 --name keeweb --hostname keeweb keeweb_nginx:1.0
+docker run -it -d --restart always -e "SSL_DOMAIN=TESTING.COM" -e "IP_WEBDAV=IP_EXTERNAL" -e "HT_USER=put_your_user_here" -e "HT_PASSWORD=put_your_password_here" -p 443:443 -p 80:80 --name keeweb --hostname keeweb keeweb_nginx:latest
